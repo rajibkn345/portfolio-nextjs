887c219b452c9b2ecc34948f6e3313079a0831f7
Roman Khan Rajib <rajibkn345@gmail.com>
2023-02-07 21:46:40 +0600

    updating

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   presets: ["next/babel"],
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+// next.config.js
+module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
